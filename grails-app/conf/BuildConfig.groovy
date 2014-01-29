e344710e934b90b270ea551323751e21e128f576
nribeka <nyoman@openmrs.org>
2014-01-29 10:25:58 -0500

    Adding the spring security and security ui dependencies.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -44,6 +44,7 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "http://repo.spring.io/milestone/"
     }
 
     dependencies {
@@ -63,11 +64,16 @@ grails.project.dependency.resolution = {
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.7" // or ":hibernate4:4.1.11.6"
         runtime ":database-migration:1.3.8"
-        runtime ":jquery:1.10.2.2"
+        runtime ":jquery:1.11.0"
         runtime ":resources:1.2.1"
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0.1"
         //runtime ":cached-resources:1.1"
         //runtime ":yui-minify-resources:0.1.5"
+        compile ":spring-security-core:2.0-RC2"
+        compile ":mail:1.0.1"
+        compile ":jquery-ui:1.10.3"
+        compile ":famfamfam:1.0.1"
+        compile ":spring-security-ui:1.0-RC1"
     }
 }
